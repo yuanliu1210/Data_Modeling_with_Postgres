0919819e5b9a9eea19bc156af6c99cfd76b225c1
root <yuanliu1210@gmail.com>
2020-05-01 22:49:21 +0000

    Sparkify Data modeling project with Postgres

---
+++ sql_queries.py
@@ -0,0 +1,117 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays \
+(songplay_id serial PRIMARY KEY,
+start_time bigint NOT NULL,
+user_id int, 
+level varchar,
+song_id varchar, 
+artist_id varchar,
+session_id int, 
+location varchar, 
+user_agent varchar)
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users 
+(user_id int PRIMARY KEY , 
+first_name varchar, 
+last_name varchar, 
+gender varchar, 
+level varchar)
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs 
+(song_id varchar PRIMARY KEY, 
+title varchar, 
+artist_id varchar, 
+year numeric, 
+duration numeric)
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists 
+(artist_id varchar PRIMARY KEY, 
+name varchar, 
+location varchar, 
+latitude numeric, 
+longitude numeric)
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time 
+(start_time timestamp, 
+hour int, 
+day int, 
+week int, 
+year int, 
+weekday int)
+""")
+
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+VALUES(%s,%s,%s,%s,%s,%s,%s,%s);
+""")
+
+user_table_insert = ("""
+INSERT INTO users  (user_id , first_name, last_name, gender, level) 
+VALUES (%s,%s,%s,%s,%s)
+ON CONFLICT(user_id) DO UPDATE
+    SET first_name = excluded.first_name,
+        last_name = excluded.last_name,
+        gender = excluded.gender,
+        level = excluded.level;
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration) 
+VALUES (%s,%s,%s,%s,%s)
+ON CONFLICT(song_id) DO UPDATE
+    SET title = excluded.title,
+        artist_id = excluded.artist_id,
+        year = excluded.year,
+        duration = excluded.duration;
+
+""" )
+
+artist_table_insert = ("""
+INSERT INTO artists (artist_id , name , location , latitude, longitude) 
+VALUES (%s,%s,%s,%s,%s)
+ON CONFLICT(artist_id) DO UPDATE
+    SET name = excluded.name,
+        location = excluded.location,
+        latitude = excluded.latitude,
+        longitude = excluded.longitude;
+
+""")
+
+
+time_table_insert = ("""
+INSERT INTO time  (start_time, hour, day, week, year, weekday) VALUES (%s,%s,%s,%s,%s,%s)
+""")
+
+# FIND SONGS
+
+song_select = ("""SELECT song_id, songs.artist_id FROM songs \
+JOIN artists ON songs.artist_id = artists.artist_id \
+WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s ;
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
